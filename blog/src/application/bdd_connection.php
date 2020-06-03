e63f4bb598c3eb8a650eb35e50260e114825cc55
Sandou-89 <christophegomis2019@gmail.com>
2020-06-03 17:04:39 +0200

    Update bdd_connection.php

--- blog/src/application/bdd_connection.php
+++ blog/src/application/bdd_connection.php
@@ -1,13 +1,10 @@
 <?php
-
-	//	Connexion à la base de données
-	$pdo = new PDO
-	(
-		'mysql:host=localhost;dbname=blog;charset=UTF8',
-		'root',
-		'',
-	    [
-	    	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-	        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-	    ]
-    );
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+?>
